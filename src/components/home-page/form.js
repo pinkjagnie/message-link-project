398c978795a13681ca92604ee571e8f71b816b67
pinkjagnie <olga.krzysztofik@gmail.com>
2023-01-20 16:47:59 +0100

    Task4: getting value from form & generating hash

--- src/components/home-page/form.js
+++ src/components/home-page/form.js
@@ -1,12 +1,27 @@
+import { useRef } from "react";
+
+import { generateHash } from "random-hash";
+
 import styles from "./form.module.css";
 
 function Form() {
+  const messageRef = useRef(null);
+
+  const messageHandler = (event) => {
+    event.preventDefault();
+
+    let hash = generateHash({ length: 8 });
+
+    console.log(messageRef.current.value)
+    console.log(hash)
+  }
+
   return (
     <section className={styles.formSection}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={messageHandler}>
         <div className={styles.control}>
           <label htmlFor="message">Your message</label>
-          <textarea id="message" rows="8" cols="50" />
+          <textarea id="message" rows="8" cols="50" required ref={messageRef} />
         </div>
         <button>Submit</button>
       </form>
