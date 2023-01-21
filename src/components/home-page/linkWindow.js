2d71432ffc444714688e5cb88c82159c15f68583
pinkjagnie <olga.krzysztofik@gmail.com>
2023-01-21 15:22:14 +0100

    Task4: copying generated link (home page)

--- src/components/home-page/linkWindow.js
+++ src/components/home-page/linkWindow.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 
 import { AiOutlineCopy } from "react-icons/ai";
@@ -5,6 +6,16 @@ import { AiOutlineCopy } from "react-icons/ai";
 import styles from "./linkWindow.module.css";
 
 function LinkWindow(props) {
+  const [copied, setCopied] = useState(false);
+
+  const copyHandler = () => {
+    navigator.clipboard.writeText(`${props.newLink}`);
+    setCopied(true);
+ 
+    const timer = setTimeout(() => {setCopied(false)}, 2000);
+    return () => clearTimeout(timer);
+  };
+
   return (
     <section className={styles.linkWindowSection}>
       <div className={styles.linkWindowSumup}>
@@ -16,7 +27,7 @@ function LinkWindow(props) {
           <span>Your link:</span>
           <Link href={props.newLink} className={styles.linkHref}>{props.newLink}</Link>
         </div>
-        <button><AiOutlineCopy /> Copy</button>
+        <button onClick={copyHandler}><AiOutlineCopy /> {!copied && "Copy"} {copied && "Copied"}</button>
       </div>
       <div className={styles.linkThanks}>Thanks for using. See you again!</div>
     </section>
