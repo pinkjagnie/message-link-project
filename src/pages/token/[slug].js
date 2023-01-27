c787454f6d231feae57fac11ee9b88a368f1c9b3
pinkjagnie <olga.krzysztofik@gmail.com>
2023-01-27 12:25:21 +0100

    Clean stuff

--- src/pages/token/[slug].js
+++ src/pages/token/[slug].js
@@ -1,8 +1,6 @@
-// import { useEffect } from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import {StyleSheet} from "@react-pdf/renderer"
-// import { PDFViewer, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
 import GeneratedPdf from "../../components/GeneratedPdf";
 
@@ -14,7 +12,7 @@ export default function Hash(props) {
   const [email, setEmail] = useState("");
   const [quantity, setQuantity] = useState("");
   const [note, setNote] = useState("");
-  const [pdf, setPdf] = useState(false);
+  const [isPdfVisible, setIsPdfVisible] = useState(false);
 
   const clickHandler = () => {
     setName(props.bills.name);
@@ -22,28 +20,13 @@ export default function Hash(props) {
     setQuantity(props.bills.quantity);
     setNote(props.bills.note)
 
-    setPdf(true)
+    setIsPdfVisible(true)
   }
-
-  // useEffect(() => {
-  //   let url = `/api/token/${props.slug}`
-
-  //   fetch(url, {
-  //     method: "DELETE",
-  //     body: JSON.stringify({
-  //       note: props.bills,
-  //       hash: props.slug
-  //     }),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  // }, [props.bills, props.slug])
 
   return(
     <div style={styles.generatedPdfSection}>
-      <button onClick={clickHandler}>Generate pdf</button>
-      {pdf && <GeneratedPdf name={name} email={email} quantity={quantity} note={note}/>}
+      {!isPdfVisible && <button style={styles.button} onClick={clickHandler}>Generate pdf</button>}
+      {isPdfVisible && <GeneratedPdf name={name} email={email} quantity={quantity} note={note}/>}
     </div>
   )
 }
@@ -86,40 +69,17 @@ const styles = StyleSheet.create({
     margin: 0,
     padding: 0,
   },
-  // pdf: {
-  //   height: '100%',
-  //   width: '100%'
-  // },
-  // page: {
-  //   backgroundColor: '#E4E4E4'
-  // },
-  // title: {
-  //   paddingTop: 40,
-  //   paddingBottom: 40,
-  //   textAlign: 'center',
-  //   fontSize: 36,
-  //   fontWeight: 'bold',
-  //   textTransform: 'uppercase',
-  //   textDecoration: 'underline'
-  // },
-  // subtitle: {
-  //   marginBottom: 8,
-  //   textDecoration: 'underline',
-  //   fontWeight: 'bold'
-  // },
-  // section: {
-  //   margin: 5,
-  //   padding: 5,
-  //   paddingLeft: 30,
-  // },
-  // sectionProducts: {
-  //   margin: 20,
-  //   padding: 10,
-  //   paddingLeft: 100,
-  // },
-  // sectionNotes: {
-  //   margin: 30,
-  //   padding: 20,
-  //   border: '1px solid black'
-  // }
+  button: {
+    display: 'block',
+    width: '50%',
+    margin: '1.2rem auto 0 auto',
+    padding: '0.5rem 2rem',
+    border: '1.5px solid whitesmoke',
+    borderRadius: '25px',
+    backgroundColor: '#656565',
+    color: 'whitesmoke',
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+  },
 });
